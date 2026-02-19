57402d3bdc8704f3dcdfc0f694e3f23d08d88de4
BhavaniShankar9273 <reachbhavanidba@gmail.com>
2026-02-19 08:21:01 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,3 @@
+// Braze service worker 
+// Must be served from the site root and with the correct content type (application/javascript) 
+self.importScripts('https://js.appboycdn.com/web-sdk/6.5/service-worker.js');
